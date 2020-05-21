39fe2eff10a4c2c708ca2797cca35624c57fc341
Катеринка <monami@phrytsen-comp>
2020-05-22 02:27:07 +0300

    :)

--- day02/ex06/ft_putnbr.c
+++ day02/ex06/ft_putnbr.c
@@ -1,12 +1,24 @@
 void ft_putchar (char c);
 
-void ft_printnbr(int nb)
+void ft_putnbr(int nb)
 {
-  
 }
 
 int main()
 {
-    ft_putnbr();
+    ft_putnbr(437);
+    ft_putchar('\n');
+    ft_putnbr(1);
+    ft_putchar('\n');
+    ft_putnbr(0);
+    ft_putchar('\n');
+    ft_putnbr(-1);
+    ft_putchar('\n');
+    ft_putnbr(-345);
+    ft_putchar('\n');
+    ft_putnbr(-2147483648);
+    ft_putchar('\n');
+    ft_putnbr(2147483647);
+    ft_putchar('\n');
     return (0);
 }
